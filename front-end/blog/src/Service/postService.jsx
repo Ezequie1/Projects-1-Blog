e3887356d398d78c80194649ba455760e0a353b2
Ezequiel <ezequielalves1021@gmail.com>
2023-11-07 15:06:04 -0300

    set variables

--- front-end/blog/src/Service/postService.jsx
+++ front-end/blog/src/Service/postService.jsx
@@ -1,26 +1,26 @@
 import axios from "axios";
-import { urlsPRD } from "../Static/variables";
+import { urls } from "../Static/variables";
 
 export function getPosts(){
-    return axios.get(urlsPRD.getAll)
+    return axios.get(urls.getAll)
 }
 
 export function createPosts(title, text){
-    return axios.post(urlsPRD.createPost, { title: title, text: text })
+    return axios.post(urls.createPost, { title: title, text: text })
 }
 
 export function searchPosts(value){
-    return axios.get(urlsPRD.searchPosts + value)
+    return axios.get(urls.searchPosts + value)
 }
 
 export function favoritePost(id){
-    return axios.put(urlsPRD.favoritePost + id)
+    return axios.put(urls.favoritePost + id)
 }
 
 export function editPost(title, text, id){
-    return axios.put(urlsPRD.editPost + id, { title: title, text: text })
+    return axios.put(urls.editPost + id, { title: title, text: text })
 }
 
 export function removePost(id){
-    return axios.delete(urlsPRD.deletePost + id)
+    return axios.delete(urls.deletePost + id)
 }
